de7846fcf8707ceb9e3d3dc558c3ed4994614a71
Botebota <chalermkhwan.b.n@gmail.com>
2020-01-27 15:03:28 -0600

    upload portfolioversion2

---
+++ resources/js/scripts.js
@@ -0,0 +1,109 @@
+$(document).ready(function(e) {
+    
+     /* For the sticky navigation */
+     $('.js--section-about').waypoint(function(direction) {
+        if (direction == "down") {
+            $('nav').addClass('sticky');
+        } else {
+            $('nav').removeClass('sticky');
+        }
+    }, {
+      offset: '60px;'
+    });
+
+    /* Scroll on buttons */
+    $('.js--scroll-to-about').click(function (e) {
+        e.preventDefault();
+        document.querySelector('.js--section-about').scrollIntoView({ behavior: 'smooth' });
+      });
+     
+      $('.js--scroll-to-project').click(function (e) {
+        e.preventDefault();
+        document.querySelector('.js--section-project').scrollIntoView({ behavior: 'smooth' });
+      });
+
+        /* Navigation scroll */
+          // Add smooth scrolling to all links
+          $("a").on('click', function(event) {
+        
+            // Make sure this.hash has a value before overriding default behavior
+            if (this.hash !== "") {
+              // Prevent default anchor click behavior
+              event.preventDefault();
+        
+              // Store hash
+              var hash = this.hash;
+        
+              // Using jQuery's animate() method to add smooth page scroll
+              // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+              $('html, body').animate({
+                scrollTop: $(hash).offset().top
+              }, 800, function(){
+        
+                // Add hash (#) to URL when done scrolling (default click behavior)
+                window.location.hash = hash;
+              });
+            } // End if
+          });
+     
+
+
+                /* Animations on scroll */
+    $('.js--wp-1').waypoint(function(direction) {
+        $('.js--wp-1').addClass('animated fadeIn');
+    }, {
+        offset: '50%'
+    });
+    
+    
+
+
+      /* Mobile navigation */
+      $('.js--nav-icon, .js--main-nav a, .logo-black').click(function(element){
+        var nav = $('.js--main-nav');
+        var icon = $('.js--nav-icon i');
+
+        //Gets the class name of the element that triggered the event
+        var clicked = element.target.className;
+
+        //Exists the function if the menu is closed AND the logo-black element (logo image) was clicked
+        if (icon.hasClass('ion-navicon-round') && clicked == 'logo-black')
+            return;
+
+        //Opens and closes the menu
+        if ($(window).width() < 768){
+            nav.slideToggle(200);
+        }
+
+        //Changes icon states of the menu button
+        if (icon.hasClass('ion-navicon-round')) {
+            icon.addClass('ion-close-round');
+            icon.removeClass('ion-navicon-round');
+        } else {
+            icon.addClass('ion-navicon-round');
+            icon.removeClass('ion-close-round');
+        }
+    });
+
+
+    $(window).resize(function(){
+        var nav = $('.js--main-nav');
+        var icon = $('.js--nav-icon i');
+
+        if ($(window).width() > 767){
+            nav.css("display", "block");
+            icon.addClass('ion-close-round');
+            icon.removeClass('ion-navicon-round');
+        } else {
+            nav.css("display", "none");
+            icon.addClass('ion-navicon-round');
+            icon.removeClass('ion-close-round');
+        }
+
+    });
+
+   
+});
+
+
+
